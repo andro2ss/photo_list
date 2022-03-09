99a2cf55abdcdf9490b15edaa266f507a04a221c
andro2ss <a.brzeski@hotmail.com>
2022-03-09 04:55:57 -0800

    created component Gallery

---
+++ src/components/appItems/gallery/Gallery.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { Pagination } from "@mui/material";
+import PhotoModal from "./photoModal/PhotoModal";
+import PhotosBox from "./photosBox/PhotosBox";
+import "./Gallery.scss";
+
+function Gallery({ photoList }) {
+  const [viewPhotos, setViewPhotos] = useState(0);
+  const [paginationPage, setPaginationPage] = useState(1);
+  const [modalPhoto, setModalPhoto] = useState({});
+
+  const handleChange = (event, value) => {
+    console.log(value);
+    setPaginationPage(value);
+    if (value === 1) {
+      setViewPhotos(0);
+    } else {
+      setViewPhotos((value - 1) * 3);
+    }
+  };
+  return (
+    <>
+      {photoList.length > 0 && (
+        <div className="photo__container">
+          <h1 className="photo__title">Gallery</h1>
+          <PhotosBox
+            photoList={photoList}
+            viewPhotos={viewPhotos}
+            setModalPhoto={setModalPhoto}
+          />
+          <Pagination
+            className="photo__pagination"
+            color="primary"
+            count={Math.ceil(photoList.length / 3)}
+            page={paginationPage}
+            onChange={handleChange}
+          />
+          <PhotoModal photo={modalPhoto} />
+        </div>
+      )}
+    </>
+  );
+}
+
+export default Gallery;
